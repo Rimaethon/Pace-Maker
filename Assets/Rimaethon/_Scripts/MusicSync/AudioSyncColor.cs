3f459bc0a06c60441f3ddca993ee7462f53e1dea
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-24 03:38:38 +0300

    Refactor AudioSyncColor to improve readability and performance
    The AudioSyncColor script has been refactored to ensure a more efficient and readable code. The 'using System.Collections.Generic' namespace has been removed as it wasn't being utilized. The script has been encapsulated within a namespace to enhance organization. Moreover, the method 'RandomColor()' has been removed and its logic incorporated within the method 'OnBeat()', eliminating an extra function call. The image's color change is now managed within a coroutine function 'MoveToColor' to prevent unwanted flicker and provide smoother color transitions.

--- Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
+++ Assets/Rimaethon/_Scripts/MusicSync/AudioSyncColor.cs
@@ -1,64 +1,52 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Image))]
-public class AudioSyncColor : AudioSyncer {
+namespace Rimaethon._Scripts.MusicSync
+{
+    [RequireComponent(typeof(Image))]
+    public class AudioSyncColor : AudioSyncer 
+    {
+        private Image imageComponent;
+        private Color[] beatColors = {Color.red, Color.blue, Color.green};
 
-	private IEnumerator MoveToColor(Color _target)
-	{
-		Color _curr = m_img.color;
-		Color _initial = _curr;
-		float _timer = 0;
-		
-		while (_curr != _target)
-		{
-			_curr = Color.Lerp(_initial, _target, _timer / timeToBeat);
-			_timer += Time.deltaTime;
+        private Color restColor = Color.white; // Default to white color
+        private int randomIndex;
 
-			m_img.color = _curr;
+        private void Start()
+        {
+            imageComponent = GetComponent<Image>();
+        }
 
-			yield return null;
-		}
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (!isBeat)
+            {
+                imageComponent.color = Color.Lerp(imageComponent.color, restColor, restSmoothTime * Time.deltaTime);
+            }
+        }
 
-		m_isBeat = false;
-	}
+        public override void OnBeat()
+        {
+            base.OnBeat();
+            StopCoroutine(MoveToColor(beatColors[randomIndex]));
+            randomIndex = Random.Range(0, beatColors.Length);
+            StartCoroutine(MoveToColor(beatColors[randomIndex]));
+        }
 
-	private Color RandomColor()
-	{
-		if (beatColors == null || beatColors.Length == 0) return Color.white;
-		m_randomIndx = Random.Range(0, beatColors.Length);
-		return beatColors[m_randomIndx];
-	}
-
-	public override void OnUpdate()
-	{
-		base.OnUpdate();
-
-		if (m_isBeat) return;
-
-		m_img.color = Color.Lerp(m_img.color, restColor, restSmoothTime * Time.deltaTime);
-	}
-
-	public override void OnBeat()
-	{
-		base.OnBeat();
-
-		Color _c = RandomColor();
-
-		StopCoroutine("MoveToColor");
-		StartCoroutine("MoveToColor", _c);
-	}
-
-	private void Start()
-	{
-		m_img = GetComponent<Image>();
-	}
-
-	public Color[] beatColors;
-	public Color restColor;
-
-	private int m_randomIndx;
-	private Image m_img;
+        private IEnumerator MoveToColor(Color target)
+        {
+            float timer = 0;
+            Color currentColor = imageComponent.color;
+            while (currentColor != target)
+            {
+                currentColor = Color.Lerp(currentColor, target, timer / timeToBeat);
+                timer += Time.deltaTime;
+                imageComponent.color = currentColor;
+                yield return null;
+            }
+            isBeat = false;
+        }
+    }
 }
