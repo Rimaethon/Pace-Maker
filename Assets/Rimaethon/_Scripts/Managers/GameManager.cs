b1977ee1dda9fe8185c36db37a0b220149202720
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-04 22:06:52 +0300

    Enable GameScene and slim down font data
    Enabled the GameScene in the project build settings to include it in the final build. The excessive and redundant information present in the Electronic Highway Sign SDF.asset file was cleaned up to reduce memory usage and disk space.
    
    This was achieved by emptying the Glyph table, Character table, Used Glyph Rects, and setting the width and height to zero, effectively making the asset lightweight. Dashboard performance should now improve due to reduced game asset size and possibly improved game scene load times with GameScene enabled in the build settings.

--- Assets/Rimaethon/_Scripts/Managers/GameManager.cs
+++ Assets/Rimaethon/_Scripts/Managers/GameManager.cs
@@ -1,25 +1,53 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using Text = UnityEngine.UI.Text;
 
 public class GameManager : MonoBehaviour
 {
-
     public static GameManager instance;
     public Text score;
+    public Text countdownText;
+    private bool isCountdownMade;
+
     private int scoreCount;
 
     private void Awake()
     {
-        if (instance != null && instance != this) Destroy(this);
+        if (instance != null && instance != this) Destroy(this.gameObject);
         else instance = this;
+        StartCoroutine(FreezeGameWithCountdown());
+
     }
+
+    private void Update()
+    {
+        score.text = "Score: " + scoreCount;
+    }
+
     public void IncrementScore()
     {
         scoreCount++;
     }
-    private void Update()
+
+    IEnumerator FreezeGameWithCountdown()
     {
-        score.text = "Score:" + scoreCount;
+        Time.timeScale = 0f; // Freezing the game
+
+        countdownText.gameObject.SetActive(true);
+
+        // Countdown from 5 to 1
+        for (int i = 5; i > 0; i--)
+        {
+            countdownText.text = i.ToString();
+            yield return new WaitForSecondsRealtime(1);
+        }
+
+        // Show "Start" text
+        countdownText.text = "Start!";
+        yield return new WaitForSecondsRealtime(1); // Keep it on screen for 1 second
+
+        // Hide countdown text and resume game
+        countdownText.gameObject.SetActive(false);
+        Time.timeScale = 1f; // Resuming the game
     }
 }
